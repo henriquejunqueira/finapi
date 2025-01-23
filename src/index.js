9005ece8733a855796042561b6c9210ad5b512b3
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2025-01-23 14:44:45 -0300

    Criado middleware para verificar se já existe uma conta com o cpf passado

--- src/index.js
+++ src/index.js
@@ -7,6 +7,25 @@ app.use(express.json());
 
 const customers = [];
 
+// Middlewares
+
+// Verifica se já existe uma conta com o cpf passado
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf == cpf);
+
+  // Se não existir um extrato (Customer) então retorna um erro 400
+  if (!customer) {
+    return response.status(400).json({ error: 'Customer not found!' });
+  }
+
+  request.customer = customer;
+
+  // Se existir um extrato (Customer) então retorna um next pra prosseguir com a requisição
+  return next();
+}
+
 // Cria uma nova conta
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
@@ -29,16 +48,13 @@ app.post('/account', (request, response) => {
   return response.status(201).send();
 });
 
+// app.use(verifyIfExistsAccountCPF); // usa o middleware criado
+
 // Busca o extrato bancário
-app.get('/statement/', (request, response) => {
-  const { cpf } = request.headers;
+app.get('/statement/', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
 
-  const customer = customers.find((customer) => customer.cpf === cpf);
-
-  if (!customer) {
-    return response.status(400).json({ error: 'Customer not found!' });
-  }
-
+  //   return response.json(customer.statement);
   return response.json(customer.statement);
 });
 
