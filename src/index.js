5ed29c02bd90d4f9ace510692ee504ec6bd1937d
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2025-01-23 16:41:53 -0300

    Criado método de depósito

--- src/index.js
+++ src/index.js
@@ -58,4 +58,20 @@ app.get('/statement/', verifyIfExistsAccountCPF, (request, response) => {
   return response.json(customer.statement);
 });
 
+// Insere depósito
+app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+  const { customer } = request;
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: 'credit',
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
 app.listen(3333);
